8911805cac15149a31ccd9a4c02eaef6aebf80ea
Dev Enthusiastic <faraazhossainimran@gmail.com>
2023-11-30 23:25:03 +0600

    css error on trendingProducts fixed

--- src/pages/Home/Trending/TrendingProducts/TrendingProducts.jsx
+++ src/pages/Home/Trending/TrendingProducts/TrendingProducts.jsx
@@ -6,7 +6,7 @@ const TrendingProducts = () => {
     const [trendingItems] = useTrending()
     console.log(trendingItems);
     return (
-        <div className="cardcontainer mx-auto my-12">
+        <div className="card container mx-auto my-12">
         <h1 className="text-4xl font-semibold md:pb-12 mb-8 m-4 ">Trending Products</h1>
       <div className="grid md:m-0 md:grid-cols-4 gap-6 m-4 ">
         {trendingItems?.map(trendingItem => <ProductCard key={trendingItem._id} productCard={trendingItem}></ProductCard>)}
