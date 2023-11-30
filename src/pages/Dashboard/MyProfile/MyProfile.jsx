6ae62269d87660414a5855726eb6ab992d8c2208
Dev Enthusiastic <faraazhossainimran@gmail.com>
2023-11-30 22:24:12 +0600

    MyProfile page design improved

--- src/pages/Dashboard/MyProfile/MyProfile.jsx
+++ src/pages/Dashboard/MyProfile/MyProfile.jsx
@@ -1,26 +1,28 @@
-import { useContext } from "react"
-import { AuthContext } from "../../../providers/AuthProvider"
+import { useContext } from "react";
+import { AuthContext } from "../../../providers/AuthProvider";
 
 const MyProfile = () => {
-    const {user} = useContext(AuthContext)
-    return (
-        <div className="m-4">
-            <h1 className="text-4xl my-4"> Information of {user?.displayName}</h1>
-            <div className="card card-compact bg-base-100">
-              <div className="my-4">
-                <div className="flex">
-                  <div className="">
-                    <div className="w-[400px]">
-                      <img src={user?.photoURL} />
-                    </div>
-                  </div>
-                </div>
-              </div>
+  const { user } = useContext(AuthContext);
+  return (
+    <div>
+      <div>
+        <div className="hero py-12 bg-base-200">
+          
+          <div className="hero-content flex-col lg:flex-row">
+            
+            <img
+              src={user?.photoURL}
+              className="max-w-sm rounded-lg shadow-2xl mr-12"
+            />
+            <div>
+              <h1 className="text-3xl">Name: {user?.displayName}</h1>
+              <h3 className="text-2xl mt-2">Email: {user?.email}</h3>
             </div>
-            <h1 className="text-3xl">Name: {user?.displayName}</h1>
-            <h3 className="text-2xl mt-2">Email: {user?.email}</h3>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default MyProfile
+export default MyProfile;
